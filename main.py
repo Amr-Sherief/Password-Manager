6a019849d491aaa32d33088db56907d3b7d7219a
Amr <sherie1165@gmail.com>
2020-11-30 23:06:08 +0200

    added the first interface for adding a Password

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+import mysql.connector
+
+mydb = mysql.connector.connect(
+
+)
 
 root = tk.Tk()
 root.attributes("-fullscreen", True)
@@ -6,23 +11,33 @@ root.title("Password Manager")
 
 val = tk.StringVar()
 
+value = val.get()
+
+frame = tk.Frame(root)
+frame.pack(side='top')
 
 options = ["Add Password", "Show Passwords", "Generate Password"]
 
 
 def check():
-    if val == options[0]:
-        print("why")
+    if val.get() == options[0]:
+        e_label = tk.Label(frame, text="Email")
+        e_label.grid(row=3, column=0, sticky='W')
+        email = tk.Entry(frame)
+        email.grid(row=3, column=1)
+        p_label = tk.Label(frame, text="Password")
+        p_label.grid(row=4, column=0, sticky='W')
+        password = tk.Entry(frame)
+        password.grid(row=4, column=1)
+        submit = tk.Button(frame, text="Submit", fg="red")
+        submit.grid(row=5, column=0, columnspan=2)
 
+
+menu = tk.OptionMenu(frame, val, *options)
+menu.grid(row=0, column=0, columnspan=2)
 
 circle = tk.PhotoImage(file="gimp-button.png")
-
-menu = tk.OptionMenu(root, val, *options, command=check())
-menu.pack()
-
-commit = tk.Button(root, image=circle, width=25, height=25, command=check())
-commit.pack()
-
-print(val)
+commit = tk.Button(frame, image=circle, width=25, height=25, command=check)
+commit.grid(row=1, column=0, columnspan=2)
 
 root.mainloop()
