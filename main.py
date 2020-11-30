cb3f07ed6ca5230072e65856bdd8a467b730c80c
Amr <sherie1165@gmail.com>
2020-11-30 19:10:23 +0200

    added a circle button

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ root.title("Password Manager")
 
 val = tk.StringVar()
 
-options = ["Add Password", "Retrieve Password", "Generate Password"]
-menu = tk.OptionMenu(root, val, *options)
+
+options = ["Add Password", "Show Passwords", "Generate Password"]
+
+
+def check():
+    if val == options[0]:
+        print("why")
+
+
+circle = tk.PhotoImage(file="gimp-button.png")
+
+menu = tk.OptionMenu(root, val, *options, command=check())
 menu.pack()
+
+commit = tk.Button(root, image=circle, width=25, height=25, command=check())
+commit.pack()
+
+print(val)
+
 root.mainloop()
